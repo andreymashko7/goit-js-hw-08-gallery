a01caf36e5b290f41deb660958d8bbbe125ef07b
andreymashko7 <63639567+andreymashko7@users.noreply.github.com>
2020-09-26 15:57:56 +0300

    добавил галерею

---
+++ task-8.js
@@ -0,0 +1,50 @@
+import transactionGallery from './gallery-items.js';
+
+// console.log(transactionGallery);
+
+// {
+//   /* <li class="gallery__item">
+//   <a
+//     class="gallery__link"
+//     href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+//   >
+//     <img
+//       class="gallery__image"
+//       src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
+//       data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+//       alt="Tulips"
+//     />
+//   </a>
+// </li>; */
+// }
+
+const galleryEl = document.querySelector('.js-gallery');
+galleryEl.addEventListener('click', onImageContainerClick);
+
+const cardsMarkup = createPhotoMarcup();
+galleryEl.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createPhotoMarcup() {
+  return transactionGallery
+    .map(imageEl => {
+      const { preview, original, description } = imageEl;
+      return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+
+function onImageContainerClick(evt) {}
+
+// console.log(galleryEl);
